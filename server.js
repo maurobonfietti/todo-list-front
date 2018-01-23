9be7948e172945ef99271233c2a2fbd7ff508a12
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-22 21:21:57 -0300

    Updates to deploy to heroku.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+
+//Install express server
 const express = require('express');
 const app = express();
 
-// Run the app by serving the static files in the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
