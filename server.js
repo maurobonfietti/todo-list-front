37ebeee1f06f3c75dc2a159704a159e16aca4c31
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-27 15:33:15 -0300

    Test: serve web app using heroku.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 
-//Install express server
 const express = require('express');
+
 const app = express();
 
-// Serve only the static files form the dist directory
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-app.get('*', function(req, res) {
-  res.sendfile('./src/index.html');
-});
