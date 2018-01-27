11b3512d3de114a6d66aa6cad72b0b27c68300fd
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-27 15:39:14 -0300

    Test: serve web app using heroku.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -19,3 +21,7 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+});
