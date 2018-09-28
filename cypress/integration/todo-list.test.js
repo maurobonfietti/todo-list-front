74c75472741db50fe062f77178b085ec1455a023
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-09-28 18:03:43 -0300

    Added Successful Login Test (E2E Test).

--- cypress/integration/todo-list.test.js
+++ cypress/integration/todo-list.test.js
@@ -9,6 +9,17 @@ describe("ToDo List Tests", () => {
     it("Login Home Page", () => {
         cy.title().should('include', 'ToDo List');
         cy.get('#btn-login').should('contain', 'Entrar');
+        cy.contains('INICIA SESIÓN');
+        cy.contains('Tu email');
+        cy.contains('Tu contraseña');
+        cy.get('#username')
+            .type('test@example.com')
+            .should('have.value', 'test@example.com');
+        cy.get('#userpass')
+            .type('12345678')
+            .should('have.value', '12345678');
+        cy.contains('Entrar').click();
+        cy.url().should('include', 'index/1');
     });
 
 });
