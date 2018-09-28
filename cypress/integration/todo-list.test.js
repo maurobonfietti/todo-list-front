d1e0fea2df3dc3f7122e30d342ab676a88902bb8
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-09-28 18:52:08 -0300

    Added Login Error Test (E2E Test).

--- cypress/integration/todo-list.test.js
+++ cypress/integration/todo-list.test.js
@@ -6,7 +6,7 @@ describe("ToDo List Tests", () => {
         cy.visit("/");
     });
 
-    it("Login Home Page", () => {
+    it("Login Successful", () => {
         cy.title().should('include', 'ToDo List');
         cy.get('#btn-login').should('contain', 'Entrar');
         cy.contains('INICIA SESIÓN');
@@ -20,6 +20,26 @@ describe("ToDo List Tests", () => {
             .should('have.value', '12345678');
         cy.contains('Entrar').click();
         cy.url().should('include', 'index/1');
+        cy.contains('tareas');
+        cy.contains('Salir');
+    });
+
+    it("Login Error", () => {
+        cy.title().should('include', 'ToDo List');
+        cy.get('#btn-login').should('contain', 'Entrar');
+        cy.contains('INICIA SESIÓN');
+        cy.contains('Tu email');
+        cy.contains('Tu contraseña');
+        cy.get('#username')
+            .type('test@example.com')
+            .should('have.value', 'test@example.com');
+        cy.get('#userpass')
+            .type('123')
+            .should('have.value', '123');
+        cy.contains('Entrar').click();
+        cy.contains('INICIA SESIÓN');
+        cy.contains('Tu email');
+        cy.contains('Tu contraseña');
     });
 
 });
